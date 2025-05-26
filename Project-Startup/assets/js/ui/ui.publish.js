c162baccf5214b4674b4b6a7e7e23a8a5e38c74d
조찬기 <36979670+isuda7@users.noreply.github.com>
2025-05-26 18:35:13 +0900

    테마 수정 완료

--- Project-Startup/assets/js/ui/ui.publish.js
+++ Project-Startup/assets/js/ui/ui.publish.js
@@ -4,10 +4,11 @@
 	- @ Init	    : 초기실행
 -------------------------------------------------------------------*/
 $(function () {
-	$("template[data-include]").each((_, el) =>
-		$(el).load($(el).data("include"), () => {
-			console.log(`[INFO] 퍼블리싱 ${$(el).data("include")} 인클루드 완료`);
+	$("[data-include]").each(function () {
+		$(this).load($(this).data("include"), function () {
+			console.log(`[INFO] 퍼블리싱 ${$(this).data("include")} 인클루드 완료`);
+			// $(this).contents().unwrap(); // 제대로 동작함
 			if (window.initUI) initUI();
-		})
-	);
+		});
+	});
 });
